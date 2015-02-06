c97653764eb972cd67020cae2f913c4d4fb64082
Joakim Børlum <joakim@roevhat.dk>
2015-02-06 10:13:44 +0100

    Kenneth, kom nu

--- web/api.php
+++ web/api.php
@@ -3,7 +3,7 @@
 header('Content-Type: application/json');
 
 $local = 'status.json';
-$remote = '../erkennethder/status.json';
+$remote = 'remote.json';
 $errorMsg = '{"status": "API FAIL: Check Arduino connection...\n"}';
 
 function saveStatus($remoteFile, $localFile) {
